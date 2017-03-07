55b62183b469d6f5ec2f904e06fff75123990fe9
ningxianjuly <gongyusun@outlook.com>
2017-03-06 21:48:14 -0800

    easy

---
+++ 20. Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() == 0) {
+            return true;
+        }
+        stack<char> stack;
+        for (int i = 0; i < s.size(); i++) {
+            if( s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stack.push(s[i]);
+            } else if ( s[i] == '}') {
+                if (!stack.empty() && stack.top() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ']') {
+                if (!stack.empty() && stack.top() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ')') {
+                if (!stack.empty() && stack.top() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            
+        }
+        return stack.empty();
+    }
+};
