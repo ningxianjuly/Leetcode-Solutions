c3b5452f2e4635739db968eed7cacb7474e27496
ningxianjuly <gongyusun@outlook.com>
2017-03-09 17:37:10 -0800

    easy

---
+++ 009. Palindrome Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x%10 == 0)) {
+            return false;
+        }
+        int sum = 0;
+        while (sum < x) {
+            sum = sum*10 + x%10;
+            x = x/10;
+        }
+        
+        return sum == x || sum/10 == x;
+    }
+};
